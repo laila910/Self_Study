4c1cd0505f32931fd15127be9b138d54b59b6020
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-16 14:46:27 +0200

    Create db.inc.php

---
+++ db.inc.php
@@ -0,0 +1,14 @@
+<?php
+$servername='localhost';
+$dbusername='root';
+$dbpassword='';
+$dbname='loginsystem';
+
+$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+
+if(!$conn){
+    die("Connection Faild :".mysqli_connect_error());
+}
+
+
+?>
