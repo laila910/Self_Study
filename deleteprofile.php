6d64930c89125da79d00b7049316d4026ac32393
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-17 18:32:18 +0200

    Create deleteprofile.php

---
+++ deleteprofile.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require "db.inc.php";
+$sessionid = $_SESSION['id'];
+
+$filename="uploads/profile".$sessionid."*";//star mean any thing write after the id thatt I search 
+$fileinfo=glob($filename);
+// print_r($fileinfo);
+
+$fileextension = explode('.',$fileinfo[0]);
+
+// print_r($fileextension);
+$fileactualextension = $fileextension[1];
+
+
+$file ="uploads/profile".$sessionid.".".$fileactualextension;
+
+if(!unlink($file)){
+  echo'file is not deleted';
+}else{
+  echo'file is deleted';
+}
+
+$sql = "UPDATE profileimg SET status=1 where userid='$sessionid ';";
+mysqli_query($conn,$sql);
